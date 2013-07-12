e8779def995f24572961e3832f4ec985f588528e
Barry vd. Heuvel <barryvdh@gmail.com>
2013-07-12 13:30:55 +0200

    Default to listen to app.debug

--- src/config/config.php
+++ src/config/config.php
@@ -5,6 +5,6 @@ return array(
 	// Set this option to false if you are already including jquery in your application to avoid conflicts.
     'jquery' => FALSE,
     
-	// Set to true to enable profiling, false to disable
-    'profiler' => TRUE
+	// Set to true to enable profiling, false to disable. Null to listen to the app.debug value (default)
+    'profiler' => null
 );
