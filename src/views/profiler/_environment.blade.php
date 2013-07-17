f98c46a6b165b44997cd1ab23c8d1bef3cc6fb07
angelside <angelside@users.sourceforge.net>
2013-07-17 14:31:40 +0300

    Add laravel version and environment data in ENV menu

--- src/views/profiler/_environment.blade.php
+++ src/views/profiler/_environment.blade.php
@@ -4,6 +4,14 @@
 		<th>Value</th>
 	</tr>
 	<tr>
+		<td>Laravel version</td>
+		<td>{{ $app::VERSION }}</td>
+	</tr>
+	<tr>
+		<td>Environment</td>
+		<td>{{ App::environment() }}</td>
+	</tr>
+	<tr>
 		<td>Timezone</td>
 		<td>{{ Config::get('app.timezone') }}</td>
 	</tr>
