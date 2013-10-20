3eb48ed8491d4693343c9be177e999a8bc014afa
Sebastian Klaus <sebastian@klaus.com.au>
2013-10-20 10:24:27 +0800

    /_profiler wildcard route now only gets registered, if urlToggle option is TRUE
    Signed-off-by: Sebastian Klaus <sebastian@klaus.com.au>

--- src/Sebklaus/routes.php
+++ src/Sebklaus/routes.php
@@ -3,17 +3,20 @@
 // Use 'before' filter to make sure all previously configured routes are still executed
 // Register new wildcard route and check if it contains '_profiler'
 $this->app->before(function() {
-	$this->app['router']->get('{path?}', function($path)
+	if ($this->app['config']->get('profiler::urlToggle'))
 	{
-		if (stristr($path, '_profiler') !== FALSE)
+		$this->app['router']->get('{path?}', function($path)
 		{
-			// Check if a '_profiler' session key exists and reverse its value
-			$state = $this->app['session']->get('_profiler') ? FALSE : TRUE;
-			// Apply new state to profiler config
-			$this->app['session']->set('_profiler', $state);
-			// Redirect back to origin
-			$origin = str_ireplace('_profiler', '', $path);
-			return $this->app['redirect']->to($origin);
-		}
-	})->where('path', '.+');
+			if (stristr($path, '_profiler') !== FALSE)
+			{
+				// Check if a '_profiler' session key exists and reverse its value
+				$state = $this->app['session']->get('_profiler') ? FALSE : TRUE;
+				// Apply new state to profiler config
+				$this->app['session']->set('_profiler', $state);
+				// Redirect back to origin
+				$origin = str_ireplace('_profiler', '', $path);
+				return $this->app['redirect']->to($origin);
+			}
+		})->where('path', '.+');
+	}
 });
