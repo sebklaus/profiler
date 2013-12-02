bffed055d0c367812430689518c4c92cb830d2c1
Sebastian Klaus <sebastian@klaus.com.au>
2013-12-02 08:40:37 +0800

    Switched to using facades
    Signed-off-by: Sebastian Klaus <sebastian@klaus.com.au>

--- src/Sebklaus/routes.php
+++ src/Sebklaus/routes.php
@@ -2,18 +2,18 @@
 
 // Use 'before' filter to make sure all previously configured routes are still executed
 // Register new wildcard route and check if it contains '_profiler'
-$this->app->before(function() {
-	if ($this->app['config']->get('profiler::urlToggle'))
+\App::before(function() {
+	if (\Config::get('profiler::urlToggle'))
 	{
 		// Register GET route
-		$this->app['router']->get('{path?}', function($path)
+		\Route::get('{path?}', function($path)
 		{
 			if (stristr($path, '_profiler') !== FALSE)
 			{
 				// Origin
 				$origin = str_ireplace('_profiler', '', $path);
 				// Check, if production is current environment and Profiler not specifically enabled
-				if (\App::environment() == 'production' && $this->app['session']->get('_profiler') !== TRUE)
+				if (\App::environment() == 'production' && \Session::get('_profiler') !== TRUE)
 				{
 					// Data to send to view
 					$data = array(
@@ -22,38 +22,38 @@ $this->app->before(function() {
 						'origin' => $origin,
 					);
 					// Show Password view
-					return View::make('profiler::profiler.password', $data);
+					return \View::make('profiler::profiler.password', $data);
 				}
 				// Resume normal Profiler enabling/ disabling
 				else
 				{
 					// Check if a '_profiler' session key exists and reverse its value
-					$state = $this->app['session']->get('_profiler') ? FALSE : TRUE;
+					$state = \Session::get('_profiler') ? FALSE : TRUE;
 					// Apply new state to profiler config
-					$this->app['session']->set('_profiler', $state);
+					\Session::set('_profiler', $state);
 					// Redirect back to origin
-					return $this->app['redirect']->to($origin);
+					return \Redirect::to($origin);
 				}
 			}
 		})->where('path', '.+');
 		// Register POST route (only in production environment)
 		if (\App::environment() == 'production')
 		{
-			$this->app['router']->post('{path?}', function($path)
+			\Route::post('{path?}', function($path)
 			{
 				if (stristr($path, '_profiler') !== FALSE)
 				{
 					// Origin
 					$origin = str_ireplace('_profiler', '', $path);
 					// Check if a '_profiler' session key exists and reverse its value
-					$state = $this->app['session']->get('_profiler') ? FALSE : TRUE;
+					$state = \Session::get('_profiler') ? FALSE : TRUE;
 					// Check, if submitted passwaord matches config password
-					if (\Hash::check(\Input::get('password'), $this->app['config']->get('profiler::urlTogglePassword'))) {
+					if (\Hash::check(\Input::get('password'), \Config::get('profiler::urlTogglePassword'))) {
 						// Apply new state to profiler config
-						$this->app['session']->set('_profiler', $state);
+						\Session::set('_profiler', $state);
 					}
 					// Redirect back to origin
-					return $this->app['redirect']->to($origin);
+					return \Redirect::to($origin);
 				}
 			})->where('path', '.+');
 		}
